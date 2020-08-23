8344ba33147c55310f1e12c8e6ab2d2a00efc9b6
eriktjardahl <erik.tjardahl+github@live.com>
2020-08-23 19:41:58 +0200

    Redefining some pins for probing and homing

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
@@ -43,13 +43,28 @@
 #define X_DIAG_PIN                         P1_29  // X-STOP
 #define Y_DIAG_PIN                         P1_28  // Y-STOP
 #define Z_DIAG_PIN                         P1_27  // Z-STOP
+#if BOTH(DELTA, SENSORLESS_HOMING)
+  #define X_MIN_PIN                        P1_26 // X-PROBE
+  #define X_MAX_PIN                        X_DIAG_PIN // X-STOP
+  #define Y_MIN_PIN                        P1_25 // Y-PROBE
+  #define Y_MAX_PIN                        Y_DIAG_PIN // Y-STOP
+  #define Z_MIN_PIN                        P1_00 // Z-PROBE
+  #define Z_MAX_PIN                        Z_DIAG_PIN // Z-STOP
+#else
+  #define X_MIN_PIN P1_29
+  #define X_MAX_PIN P1_26
+  #define Y_MIN_PIN P1_28
+  #define Y_MAX_PIN P1_25
+  #define Z_MIN_PIN P1_27
+  #define Z_MAX_PIN P1_00
+#endif
 #define E0_DIAG_PIN                        P1_26  // E0DET
 #define E1_DIAG_PIN                        P1_25  // E1DET
 
 //
 // Limit Switches
 //
-#ifdef X_STALL_SENSITIVITY
+/*#ifdef X_STALL_SENSITIVITY
   #define X_STOP_PIN                  X_DIAG_PIN
   #if X_HOME_DIR < 0
     #define X_MAX_PIN                      P1_26  // E0DET
@@ -83,6 +98,7 @@
     #define Z_STOP_PIN                     P1_27  // Z-STOP
   #endif
 #endif
+*/
 
 //
 // Z Probe (when not Z_MIN_PIN)
