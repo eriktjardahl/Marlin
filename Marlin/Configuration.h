13ceffe32f0aa055c334efe51dac20d28ec5e05b
eriktjardahl <erik.tjardahl+github@live.com>
2020-09-09 20:58:06 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -697,7 +697,7 @@
   #endif
 
   // Distance between bed and nozzle Z home position
-  #define DELTA_HEIGHT 315.00             // (mm) Get this value from G33 auto calibrate
+  #define DELTA_HEIGHT 258.64             // (mm) Get this value from G33 auto calibrate
 
   #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // Get these values from G33 auto calibrate
 
